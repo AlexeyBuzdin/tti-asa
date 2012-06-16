fbe2f2d225acb77f66371e0d1959ae874d8fc5d7
Alexey <alex.buzdin@gmail.com>
2012-06-16 14:29:26 +0300

    initial commit

---
+++ src/lv/tti/app/ScheduleActivity.java
@@ -0,0 +1,13 @@
+package lv.tti.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScheduleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
